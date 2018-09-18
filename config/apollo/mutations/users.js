077dd39545db51bc32470f4d9bc6b49da92109ab
austinoboyle <14arob@queensu.ca>
2018-09-17 20:55:14 -0400

    Add auth to user mutations

--- config/apollo/mutations/users.js
+++ config/apollo/mutations/users.js
@@ -1,6 +1,10 @@
 const User = require("../../../models/User");
+const { isAccountOwner, requirePermission } = require("../authTypes");
 
 exports.createUser = (obj, { username }) => User.create({ username });
 
-exports.deleteUser = (obj, { user_id }, context) =>
-    User.remove({ _id: user_id });
+exports.deleteUser = (obj, { user_id }, context) => {
+    return requirePermission(isAccountOwner(context.user, user_id)).then(() =>
+        User.remove({ _id: user_id })
+    );
+};
