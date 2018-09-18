1180c75c62ca5d2e80c8607e9818ff9d46a832a0
austinoboyle <14arob@queensu.ca>
2018-09-18 13:58:23 -0400

    Add some utility functions/data for testing

---
+++ config/tests/testUtils.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const resetDb = require("../../resetDb");
+const { schema } = require("../apollo");
+const { graphql } = require("graphql");
+
+exports.setup = done => {
+    resetDb().then(() => done());
+};
+
+exports.end = done => {
+    mongoose.disconnect().then(() => done());
+};
+
+exports.graphQLQuery = (query, context = null) => {
+    return graphql(schema, query, null, context);
+};
+
+exports.mutation = q => `mutation{${q}}`;
+exports.query = q => `query{${q}}`;
+
+exports.admin_id = "111111111111111111111111";
+exports.user_id = "111111111111111111111112";
+exports.owner_ids = [
+    "111111111111111111111114",
+    "111111111111111111111115",
+    "111111111111111111111116"
+];
+exports.shop_ids = [
+    "211111111111111111111111",
+    "211111111111111111111112",
+    "211111111111111111111113"
+];
