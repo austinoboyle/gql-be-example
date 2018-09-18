698740cfa7acf5ff1ffbcac2af398053d7f1db0f
austinoboyle <14arob@queensu.ca>
2018-09-17 20:56:03 -0400

    Add auth to shop mutations

--- config/apollo/mutations/shops.js
+++ config/apollo/mutations/shops.js
@@ -1,28 +1,34 @@
 const Shop = require("../../../models/Shop");
 const User = require("../../../models/User");
+const { isUser, isStoreOwner, requirePermission } = require("../authTypes");
 
 exports.createShop = (obj, { name }, context) => {
-    return Shop.create({
-        name,
-        owners: [context.user._id.toString()]
-    })
-        .then(newShop => {
-            return Shop.populate(newShop, [
-                {
-                    path: "owners"
-                }
-            ]);
+    return requirePermission(isUser(context.user)).then(() =>
+        Shop.create({
+            name,
+            owners: [context.user._id.toString()]
         })
-        .then(populatedShop => {
-            if (context.user.access === "USER") {
-                return User.findByIdAndUpdate(context.user._id.toString(), {
-                    access: "STOREOWNER"
-                }).then(() => populatedShop);
-            } else {
-                return populatedShop;
-            }
-        });
+            .then(newShop => {
+                return Shop.populate(newShop, [
+                    {
+                        path: "owners"
+                    }
+                ]);
+            })
+            .then(populatedShop => {
+                if (context.user.access === "USER") {
+                    return User.findByIdAndUpdate(context.user._id.toString(), {
+                        access: "STOREOWNER"
+                    }).then(() => populatedShop);
+                } else {
+                    return populatedShop;
+                }
+            })
+    );
 };
 
-exports.deleteShop = (obj, { shop_id }, context) =>
-    Shop.remove({ _id: shop_id });
+exports.deleteShop = (obj, { shop_id }, context) => {
+    return requirePermission(isStoreOwner(context.user, shop_id)).then(() =>
+        Shop.remove({ _id: shop_id })
+    );
+};
