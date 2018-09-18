87acbced65a72ef2511623d68419671c47939d67
austinoboyle <14arob@queensu.ca>
2018-09-17 20:57:36 -0400

    Add auth to product mutations

--- config/apollo/mutations/products.js
+++ config/apollo/mutations/products.js
@@ -1,18 +1,21 @@
 const Product = require("../../../models/Product");
 const Shop = require("../../../models/Shop");
+const { isStoreOwner, requirePermission } = require("../authTypes");
 const { createQuery } = require("../../../utils");
 
 exports.createProduct = (obj, { name, price, shop_id }, context) => {
-    return Product.create({
-        name,
-        price,
-        shop: shop_id
-    }).then(newProduct => {
-        return Shop.findOneAndUpdate(
-            { _id: shop_id },
-            { $push: { products: newProduct._id.toString() } }
-        ).then(() => newProduct);
-    });
+    return requirePermission(isStoreOwner(context.user, shop_id)).then(() =>
+        Product.create({
+            name,
+            price,
+            shop: shop_id
+        }).then(newProduct => {
+            return Shop.findOneAndUpdate(
+                { _id: shop_id },
+                { $push: { products: newProduct._id.toString() } }
+            ).then(() => newProduct);
+        })
+    );
 };
 
 exports.updateProduct = (
@@ -20,19 +23,24 @@ exports.updateProduct = (
     { shop_id, product_id, name, price },
     context
 ) => {
-    return Product.findOneAndUpdate(
-        { _id: product_id, shop: shop_id },
-        createQuery({ name, price }),
-        { new: true }
-    )
-        .exec()
-        .then(updated => {
-            if (updated === null) {
-                throw new Error("Product does not exist");
-            }
-            return updated;
-        });
+    return requirePermission(isStoreOwner(context.user, shop_id)).then(() =>
+        Product.findOneAndUpdate(
+            { _id: product_id, shop: shop_id },
+            createQuery({ name, price }),
+            { new: true }
+        )
+            .exec()
+            .then(updated => {
+                if (updated === null) {
+                    throw new Error("Product does not exist");
+                }
+                return updated;
+            })
+    );
 };
 
-exports.deleteProduct = (obj, { shop_id, product_id }, context) =>
-    Product.remove({ _id: product_id, shop: shop_id });
+exports.deleteProduct = (obj, { shop_id, product_id }, context) => {
+    return requirePermission(isStoreOwner(context.user, shop_id)).then(() =>
+        Product.remove({ _id: product_id, shop: shop_id })
+    );
+};
