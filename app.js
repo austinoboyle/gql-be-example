fda44530b9ebebd4da1c054af960352c0ba5acc0
austinoboyle <14arob@queensu.ca>
2018-09-21 19:41:54 -0400

    Add docs to root app

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/** @module app */
 const express = require("express");
 const mongoose = require("mongoose");
 const { ApolloServer } = require("apollo-server-express");
@@ -7,11 +8,23 @@ const { schema } = require("./config/apollo");
 const resetDb = require("./resetDb");
 db();
 
+/**
+ * Parse an authorization header token
+ *
+ * @param {String} token token to be parsed
+ * @returns {String} Mongo _id of user based on token
+ */
 const readToken = token => {
     // Do things
     return token;
 };
 
+/**
+ * Create the context for each GraphQL Request
+ *
+ * @param {*} request { req: request object }
+ * @returns {Object} {user: Object|null}
+ */
 const handleContext = ({ req }) => {
     const token = req.headers.authorization || "";
     const user_id = readToken(token);
@@ -24,12 +37,15 @@ const handleContext = ({ req }) => {
 
 const server = new ApolloServer({
     schema,
+    // make playground show up in production
     playground: true,
     uploads: false,
     context: handleContext
 });
 
 const app = express();
+
+// Use to restore the database back to the seed data.
 app.get("/resetDb", (req, res) => {
     resetDb().then(
         success => (success ? res.send("Success!") : res.send("FAIL!"))
