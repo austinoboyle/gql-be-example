923f4f70663417b593dec61d24541fa3088d6b5f
austinoboyle <14arob@queensu.ca>
2018-09-21 16:49:35 -0400

    Add conditional MONGO_CONNECTION_STRING logic to environments file

--- config/environments.js
+++ config/environments.js
@@ -1,28 +1,18 @@
 require("dotenv-flow").config();
-const MONGO_CONNECTION_STRING = `mongodb://${process.env.MONGO_USER}:${
-    process.env.MONGO_PASS
-}@${process.env.MONGO_URL}`;
+let MONGO_CONNECTION_STRING = "mongodb://127.0.0.1/graphql";
+if (process.env.MONGO_URL !== undefined) {
+    MONGO_CONNECTION_STRING = `mongodb://${process.env.MONGO_USER}:${
+        process.env.MONGO_PASS
+    }@${process.env.MONGO_URL}`;
+}
 console.log("CONNECTION STRING", MONGO_CONNECTION_STRING);
+const baseConfig = {
+    app: { name: "graphql-api" },
+    db: MONGO_CONNECTION_STRING
+};
 module.exports = {
     //MongoDB configuration
-    development: {
-        db: "mongodb://127.0.0.1/graphql",
-        app: {
-            name: "graphql"
-        }
-    },
-    test: {
-        db: process.env.REMOTE_TEST_DB
-            ? MONGO_CONNECTION_STRING
-            : "mongodb://127.0.0.1/graphql-test",
-        app: {
-            name: "graphql"
-        }
-    },
-    production: {
-        db: MONGO_CONNECTION_STRING,
-        app: {
-            name: "graphql"
-        }
-    }
+    development: baseConfig,
+    test: baseConfig,
+    production: baseConfig
 };
